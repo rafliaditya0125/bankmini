c12f68b267ae39730cff1e5a176f0a3727c83cd8
rafliaditya0125 <rafliaditya0125@gmail.com>
2026-09-09 10:01:10 +0700

    fix: perbaiki masalah transaksi yang dibatalkan masuk ke laporan transaksi

--- resources/views/exports/transactions.blade.php
+++ resources/views/exports/transactions.blade.php
@@ -254,15 +254,13 @@
         <table class="data-table">
             <thead>
                 <tr>
-                    <th style="width: 10%;">KODE SLIP</th>
-                    <th style="width: 9%;">TANGGAL</th>
-                    <th style="width: 20%;">IDENTITAS NASABAH</th>
-                    <th style="width: 8%;">AKSI</th>
-                    <th style="width: 11%;">PETUGAS</th>
-                    <th style="width: 12%;">SALDO AWAL</th>
-                    <th style="width: 12%;">SALDO AKHIR</th>
-                    <th style="width: 9%;">DEBIT</th>
-                    <th style="width: 9%;">KREDIT</th>
+                    <th style="width: 14%;">KODE SLIP</th>
+                    <th style="width: 11%;">TANGGAL</th>
+                    <th style="width: 27%;">IDENTITAS NASABAH</th>
+                    <th style="width: 10%;">AKSI</th>
+                    <th style="width: 14%;">PETUGAS</th>
+                    <th style="width: 12%;">DEBIT</th>
+                    <th style="width: 12%;">KREDIT</th>
                 </tr>
             </thead>
             <tbody>
@@ -354,14 +352,12 @@
                     </td>
                     <td class="text-center">{{ $aksi }}</td>
                     <td class="text-center">{{ $petugasName }}</td>
-                    <td class="text-right">{{ number_format($row->saldo_sebelum ?? 0, 0, ',', '.') }}</td>
-                    <td class="text-right">{{ number_format($row->saldo_sesudah ?? 0, 0, ',', '.') }}</td>
                     <td class="text-right">{{ $debit > 0 ? number_format($debit, 0, ',', '.') : '-' }}</td>
                     <td class="text-right">{{ $kredit > 0 ? number_format($kredit, 0, ',', '.') : '-' }}</td>
                 </tr>
                 @empty
                 <tr>
-                    <td colspan="9" class="text-center">Tidak ada data transaksi</td>
+                    <td colspan="7" class="text-center">Tidak ada data transaksi</td>
                 </tr>
                 @endforelse
             </tbody>
